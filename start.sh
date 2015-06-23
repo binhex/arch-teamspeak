01ff4b4703c26147cf9432ba477aa520183a9a02
kahatie <kahatie@gmail.com>
2015-06-23 22:38:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ install_path="/usr/share/teamspeak3-server"
 db_path="$install_path/sql/ts3server.sqlitedb"
 
 # create variables for files path in docker container
-files_path="$install_path/files/"
+files_path="$install_path/files"
 
 # if db DOES NOT exist in docker container and db DOES exist on /config then symlink to db
 if [ ! -f $db_path ] && [ -f /config/ts3server.sqlitedb ]
@@ -29,9 +29,29 @@ then
     ln -s /config/ts3server.sqlitedb $db_path
 fi
 
-# files data
-rm $files_path
-ln -s /config/files $files_path
+### FILES ###
+# same for files folder used for files transfet
+
+# if files DOES NOT exist in docker container and files DOES exist on /config then symlink to files
+if [ ! -f $files_path ] && [ -f /config/files ]
+then
+    ln -s /config/files $files_path
+fi
+
+# if files DOES exist in docker container and DOES NOT exist on /config then move to /config and symlink to files on /config
+if [ -f $files_path ] && [ ! -f /config/files ]
+then
+    mv $files_path /config/files
+    ln -s /config/files $files_path
+fi
+
+# if files DOES exist in docker container and DOES exist on /config then delete files in container and symlink to files on /config
+if [ -f $files_path ] && [ -f /config/files ]
+then
+    rm $files_path
+    ln -s /config/files $files_path
+fi
+
 
 # change dir to sql files
 cd /usr/share/teamspeak3-server/sql/
